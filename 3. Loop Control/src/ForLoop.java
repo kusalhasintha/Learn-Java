918b9d9f4936c6b8fd1b822de2ee8337f71e39cb
SithumPathirana <sithumkaushitha@gmail.com>
2017-10-24 19:22:25 +0530

    For Loop

---
+++ 3. Loop Control/src/ForLoop.java
@@ -0,0 +1,9 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        System.out.println("Natural Numbers form 1 to 100");
+
+        for(int i=1;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
