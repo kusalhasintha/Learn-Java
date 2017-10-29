5af52a41b06db67e2273196e07c13894a41400d8
Kusal Hasintha Kithmal Kahaduwa <kusalhasintha2012@gmail.com>
2017-10-29 13:59:57 +0530

    done by kusalhasintha!

---
+++ 3. Loop Control/src/ForLoop.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter starting number:");
+        int startingNo= input.nextByte();
+        System.out.print("Enter ending number:");
+        int endingNo= input.nextByte();
+
+        for (;startingNo<=endingNo;startingNo++){
+
+            System.out.println(startingNo);
+        }
+    }
+}
